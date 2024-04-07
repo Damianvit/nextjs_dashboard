6a6cc5b7b56ed3003728772a00ec27af8b0f7ed8
Damianvit <damianvitchib@gmail.com>
2024-04-07 15:23:36 +0100

    dev vs production behavior

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,3 +1,5 @@
+import { unstable_noStore as noStore } from 'next/cache';
+
 import { sql } from '@vercel/postgres';
 import {
   CustomerField,
@@ -11,9 +13,7 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
